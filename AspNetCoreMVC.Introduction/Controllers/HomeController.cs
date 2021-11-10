508cb83133d3b1760838e81ad742a1e1c9a80b54
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-10 17:54:33 +0300

    Models

--- AspNetCoreMVC.Introduction/Controllers/HomeController.cs
+++ AspNetCoreMVC.Introduction/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreMVC.Introduction.Entities;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,25 @@ namespace AspNetCoreMVC.Introduction.Controllers
         public ViewResult Index2()
         {
             return View();
+        }
+
+        public ViewResult Index3()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee{Id=1,FirstName="Ünsal",LastName="Şentürk",CityId=34},
+                new Employee{Id=2,FirstName="Yunus",LastName="Şentürk",CityId=34},
+                new Employee{Id=3,FirstName="Emre",LastName="Şentürk",CityId=34}
+            };
+
+            List<string> cities = new List<string> { "İstanbul", "Ankara" };
+
+            var model = new EmployeeListViewModel
+            {
+                Employees = employees,
+                cities = cities
+            };
+            return View(model);
         }
     }
 }
---
+++ AspNetCoreMVC.Introduction/Models/EmployeeListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCoreMVC.Introduction.Entities;
+using System.Collections.Generic;
+
+namespace AspNetCoreMVC.Introduction
+{
+    public class EmployeeListViewModel
+    {
+        public List<Employee> Employees { get; set; }
+        public List<string> cities { get; set; }
+    }
+}
