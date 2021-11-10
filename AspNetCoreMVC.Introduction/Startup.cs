97621e769a73d5d146c9211ae127ff261e04f7f7
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-10 17:15:56 +0300

    controller | middleware configuration
    Startup yapılandırması.
    Use kelimesi middleware eklendini gösteriyor.

--- AspNetCoreMVC.Introduction/Startup.cs
+++ AspNetCoreMVC.Introduction/Startup.cs
@@ -16,6 +16,7 @@ namespace AspNetCoreMVC.Introduction
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,17 +25,15 @@ namespace AspNetCoreMVC.Introduction
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
+            }           
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
+            app.UseMvc(routes =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=home}/{action=index}/{id?}");
             });
+           
         }
     }
 }
