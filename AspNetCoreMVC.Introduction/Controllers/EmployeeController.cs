edcc3ef2f6facb43dc88f35b0aa4d0905c541fa0
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-11 19:43:41 +0300

    Form ve Input Tag Helpers

---
+++ AspNetCoreMVC.Introduction/Controllers/EmployeeController.cs
@@ -0,0 +1,27 @@
+﻿using AspNetCoreMVC.Introduction.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC.Introduction.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Add()
+        {
+            var employeeAddViewModel = new EmployeeAddViewModel
+            {
+                Employee = new Employee()
+            };
+            return View(employeeAddViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Employee employee)
+        {
+            return View();
+        }
+    }
+}
---
+++ AspNetCoreMVC.Introduction/Models/EmployeeAddViewModel.cs
@@ -0,0 +1,9 @@
+﻿using AspNetCoreMVC.Introduction.Entities;
+
+namespace AspNetCoreMVC.Introduction
+{
+    public class EmployeeAddViewModel
+    {
+        public Employee Employee { get; set; }
+    }
+}
