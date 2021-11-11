9f8bb2eeef58a832454c2dcd9b7bb4acb0d3a0de
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-11 21:37:45 +0300

    Attribute Routing

---
+++ AspNetCoreMVC.Introduction/Controllers/AdminController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC.Introduction.Controllers
+{
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        [Route("")]
+        [Route("save")]
+        [Route("~/save")] //önündeki herşeyi iptal et
+        public string Save()
+        {
+            return "Saved";
+        }
+        [Route("delete/{id?}")]
+        public string Deleted(int id=0)
+        {
+            return String.Format("Deleted {0}", id);
+        }
+        [Route("update")]
+        public string Update()
+        {
+            return "Updated";
+        }
+    }
+}
