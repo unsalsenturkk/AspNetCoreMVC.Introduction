0bdcacba98cdc3a71d090beb3ab2c482feb98208
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-16 19:24:59 +0300

    View Components

---
+++ AspNetCoreMVC.Introduction/Models/StudentListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using AspNetCoreMVC.Introduction.Entities;
+using System.Collections.Generic;
+
+namespace AspNetCoreMVC.Introduction
+{
+    public class StudentListViewModel
+    {
+        public List<Student> Students { get; set; }
+    }
+}
---
+++ AspNetCoreMVC.Introduction/ViewComponents/StudentListViewComponent.cs
@@ -0,0 +1,28 @@
+﻿using AspNetCoreMVC.Introduction.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC.Introduction.ViewComponents
+{
+    public class StudentListViewComponent : ViewComponent
+    {
+        private readonly SchoolContext _context;
+
+        public StudentListViewComponent(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public ViewViewComponentResult Invoke()
+        {
+            return View(new StudentListViewModel
+            {
+                Students = _context.Students.ToList()
+            }); ;
+        }
+    }
+}
