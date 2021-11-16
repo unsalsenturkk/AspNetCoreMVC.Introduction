35ad622b1c2240b2ad374cf54dd916b1ca523b29
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-16 20:52:59 +0300

    Exception Filtresi Yazmak

--- AspNetCoreMVC.Introduction/Controllers/HomeController.cs
+++ AspNetCoreMVC.Introduction/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using AspNetCoreMVC.Introduction.Entities;
+using AspNetCoreMVC.Introduction.Filters;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -14,6 +15,7 @@ namespace AspNetCoreMVC.Introduction.Controllers
             return "Hello from first application";
         }
 
+        [HandleException]
         public ViewResult Index2()
         {
             throw new Exception("Some exception occured!");
---
+++ AspNetCoreMVC.Introduction/Filters/HandleExceptionAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC.Introduction.Filters
+{
+    public class HandleExceptionAttribute : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+
+            var result = new ViewResult { ViewName = "Error" };
+            var modelDataProvider = new EmptyModelMetadataProvider();
+            result.ViewData = new ViewDataDictionary(modelDataProvider, context.ModelState);
+
+            result.ViewData.Add("HandleException", context.Exception);
+            context.Result = result;
+            context.ExceptionHandled = true;
+        }
+    }
+}
