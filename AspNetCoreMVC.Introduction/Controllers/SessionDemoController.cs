c707cadd77b7561e909e7586c1b75815321d0cce
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-16 23:08:28 +0300

    Session Nesnesinin Genişletilmesi

--- AspNetCoreMVC.Introduction/Controllers/SessionDemoController.cs
+++ AspNetCoreMVC.Introduction/Controllers/SessionDemoController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using AspNetCoreMVC.Introduction.Entities;
+using AspNetCoreMVC.Introduction.ExtensionMethods;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,12 +15,14 @@ namespace AspNetCoreMVC.Introduction.Controllers
         {
             HttpContext.Session.SetInt32("age", 32);
             HttpContext.Session.SetString("name", "Ünsal");
+            HttpContext.Session.SetObject("student", new Student { Email = "abc@gmail.com", FirstName = "abc", LastName = "vyz", Id = 99 });
             return "Session created";
         }
 
         public string GetSessions()
         {
-            return String.Format("Hello {0},you are {1}", HttpContext.Session.GetString("name"), HttpContext.Session.GetInt32("age"));
+            return String.Format("Hello {0},you are {1}. Studen is {2}", HttpContext.Session.GetString("name"), HttpContext.Session.GetInt32("age"),
+              HttpContext.Session.GetObject<Student>("student").FirstName);
         }
     }
 }
