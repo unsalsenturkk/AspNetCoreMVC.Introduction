2500eaf61860d8393f2984e4d1176cd6b03390df
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-17 21:48:28 +0300

    Login Aksiyonlarının Yazılması

---
+++ AspNetCoreMVC.Introduction/Controllers/SecurityController.cs
@@ -0,0 +1,58 @@
+﻿using AspNetCoreMVC.Introduction.Identity;
+using AspNetCoreMVC.Introduction.Models.Security;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC.Introduction.Controllers
+{
+    public class SecurityController : Controller
+    {
+        private UserManager<AppIdentityUser> _userManager;
+        private SignInManager<AppIdentityUser> _signInManager;
+
+        public SecurityController(UserManager<AppIdentityUser> userManager,SignInManager<AppIdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(loginViewModel);
+            }
+
+            var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+            if (user!=null)
+            {
+                if (!await _userManager.IsEmailConfirmedAsync(user))
+                {
+                    ModelState.AddModelError(String.Empty, "Confirm your email please");
+                    return View(loginViewModel);
+                }
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Student");
+            }
+
+            ModelState.AddModelError(String.Empty, "Login failed");
+            return View(loginViewModel);
+
+        }
+    }
+}
