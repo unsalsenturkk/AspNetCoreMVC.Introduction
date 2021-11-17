ae4d65e6028147b6f36a5c5e1b05b35c2bf4809b
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-17 21:02:12 +0300

    ViewModel Nesnelerinin Oluşturulması

---
+++ AspNetCoreMVC.Introduction/Models/Security/ResetPasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreMVC.Introduction.Models.Security
+{
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        public string Code { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string ConfirmedPassword { get; set; }
+    }
+}
