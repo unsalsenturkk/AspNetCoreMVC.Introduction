fcbfda67078cdfc5615edc573bb671cb22952e98
unsalsenturkk <unsalsenturkk@gmail.com>
2021-11-17 23:59:41 +0300

    Register Görünümünün Yazılması ve Test Edilmesi

--- AspNetCoreMVC.Introduction/Startup.cs
+++ AspNetCoreMVC.Introduction/Startup.cs
@@ -48,10 +48,10 @@ namespace AspNetCoreMVC.Introduction
             services.Configure<IdentityOptions>(options =>
             {
                 options.Password.RequireDigit = true;
-                options.Password.RequireLowercase = true;
+                options.Password.RequireLowercase = false;
                 options.Password.RequiredLength = 6;
-                options.Password.RequireNonAlphanumeric = true;
-                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
 
                 options.Lockout.MaxFailedAccessAttempts = 5;
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
